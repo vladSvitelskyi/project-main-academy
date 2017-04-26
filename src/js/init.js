6f7a5db0855a4fe7974effe85188a9a5e263cb45
Vladyslav Svitelskyi <Vlad.Svitelckyi@gmail.com>
2017-04-27 00:06:30 +0300

    add slider

--- src/js/init.js
+++ src/js/init.js
@@ -1,8 +1,10 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.slider').slider({indicators: true, transitions: 500, interval: 5000});
+        $('.carousel').carousel({dist: -90});
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
